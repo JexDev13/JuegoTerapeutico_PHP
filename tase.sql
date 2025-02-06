cece6ff9c05a1ed59981fcbb432aae00dcd9302c
JexDev13 <jeremy.arias@epn.edu.ec>
2025-02-05 22:44:48 -0500

    Script de creación bd

---
+++ tase.sql
@@ -0,0 +1,167 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-02-2025 a las 04:43:45
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estadisticas`
+--
+
+CREATE TABLE `estadisticas` (
+  `id` int(11) NOT NULL,
+  `pacienteId` int(11) DEFAULT NULL,
+  `tiempoJuego` time DEFAULT NULL,
+  `movimientos` int(11) DEFAULT NULL,
+  `evaluacion` varchar(50) DEFAULT NULL,
+  `tipoJuego` varchar(50) DEFAULT NULL,
+  `dificultad` varchar(50) DEFAULT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `estadisticas`
+--
+
+INSERT INTO `estadisticas` (`id`, `pacienteId`, `tiempoJuego`, `movimientos`, `evaluacion`, `tipoJuego`, `dificultad`, `fecha`) VALUES
+(1, 1, '00:15:00', 8, 'Excelente', 'Pesos menor a mayor', 'Fácil', '2025-02-06 03:40:52'),
+(2, 1, '00:08:00', 7, 'Excelente', 'Ordenar palabras', 'Difícil', '2025-02-06 03:41:07'),
+(3, 3, '00:10:00', 9, 'Excelente', 'Pesos mayor a menor', 'Fácil', '2025-02-06 03:41:33');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `id` int(11) NOT NULL,
+  `cedula` varchar(10) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `fechaNacimiento` date NOT NULL,
+  `terapeutaId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `paciente`
+--
+
+INSERT INTO `paciente` (`id`, `cedula`, `nombre`, `apellido`, `fechaNacimiento`, `terapeutaId`) VALUES
+(1, '1711548592', 'Carlos', 'Perez', '1978-08-09', 1),
+(2, '1710458796', 'Andrés', 'Paredes', '1980-03-12', 1),
+(3, '1720395061', 'María', 'Gómez', '1976-07-05', 1),
+(4, '1730564218', 'Juan', 'Rodríguez', '1972-09-30', 1),
+(5, '1710235694', 'Ana', 'López', '1980-11-08', 1),
+(6, '1740587412', 'Carlos R.', 'Martínez', '1979-02-03', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `contraseña` varchar(255) NOT NULL,
+  `isactive` tinyint(1) DEFAULT 1,
+  `dt` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `nombre`, `apellido`, `correo`, `contraseña`, `isactive`, `dt`) VALUES
+(1, 'Jeremy', 'Arias', 'jeremyismael20009@gmail.com', '$2y$10$0F5WVnE4e1ZQ7vQukgepm.zZEA2xhjox5vr9A6SZcZ9e1iKXboBnu', 1, '2025-02-05 22:33:18');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `estadisticas`
+--
+ALTER TABLE `estadisticas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pacienteId` (`pacienteId`);
+
+--
+-- Indices de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `cedula` (`cedula`),
+  ADD KEY `terapeutaId` (`terapeutaId`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `estadisticas`
+--
+ALTER TABLE `estadisticas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `estadisticas`
+--
+ALTER TABLE `estadisticas`
+  ADD CONSTRAINT `estadisticas_ibfk_1` FOREIGN KEY (`pacienteId`) REFERENCES `paciente` (`id`);
+
+--
+-- Filtros para la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  ADD CONSTRAINT `paciente_ibfk_1` FOREIGN KEY (`terapeutaId`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
